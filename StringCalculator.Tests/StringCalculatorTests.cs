116df622776bcb1e2b27b83136e7dd0a78bfdf34
MikolHutnik <mikolhutnik@gmail.com>
2018-07-09 09:46:57 -0400

    first commit

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{[TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_Returns_0_For_Empty_String()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+
+    }
+}
